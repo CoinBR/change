3ff1efec08af58f1d0be76c7130a0d0bd93bf7a3
Pedro 'Coin' Duarte <coinbr@gmail.com>
2020-08-10 17:22:06 -0300

    Put ToPays in its own Component

---
+++ app/src/components/ToPays.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div v-for="toPay in toPays" :key="toPay.i">
+  </div>
+</div>
+</template>
+
+<script>
+
+function getResetToPays(numOfFields){
+  return [...Array(numOfFields).keys()].map( (i) => {
+    return { 
+      i: i + 1,
+      value: 0
+    }
+  })
+}
+
+export default {
+  name: 'ToPays',
+  
+  props: {
+    numberOfToPayFields: Number,
+  },
+ 
+  data(){ return {
+    fields: getResetToPays(this.numberOfToPayFields),
+  }}
+}
+</script>
--- app/tests/unit/ToPays.spec.js
+++ app/tests/unit/ToPays.spec.js
@@ -1,26 +1,26 @@
 import { mount } from '@vue/test-utils'
-import GiveChange from '@/components/GiveChange.vue'
+import ToPays from '@/components/ToPays.vue'
 
 
 
-describe('GiveChange', () => {
+describe('ToPays', () => {
   it('has data.toPays set to an array of objs with indexes and values the costumer has to pay', () => {
-    const wrapper = mount(GiveChange, {
+    const wrapper = mount(ToPays, {
       propsData: { numberOfToPayFields: 4 }
     })
 
-    expect(wrapper.vm.toPays).toEqual([
+    expect(wrapper.vm.fields).toEqual([
       { i: 1, value: 0 },
       { i: 2, value: 0 },
       { i: 3, value: 0 },
       { i: 4, value: 0 }
     ])
 
-    const wrapper7 = mount(GiveChange, {
+    const wrapper7 = mount(ToPays, {
       propsData: { numberOfToPayFields: 7 }
     })
 
-    expect(wrapper7.vm.toPays).toEqual([
+    expect(wrapper7.vm.fields).toEqual([
       { i: 1, value: 0 },
       { i: 2, value: 0 },
       { i: 3, value: 0 },
